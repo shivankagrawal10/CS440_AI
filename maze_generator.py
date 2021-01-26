f4b860a054cfd69855eeb13982c73d6b07668aa2
Sam Malnati <sjmalnati@hotmail.com>
2021-01-26 15:00:31 -0500

    temp

--- maze_generator.py
+++ maze_generator.py
@@ -6,10 +6,11 @@ class maze():
         self.maze=np.zeros((dim,dim))
         for i in range(dim):
             for j in range(dim):
-                if (not i == 0 and j == 0) and (not i == dim-1 and not j == dim-1)
-                    and random() <= p:
+                if random.random() <= p:
                     self.maze[i][j] = 1
+        self.maze[0][0] = 0
+        self.maze[dim-1][dim-1] = 0
         print(self.maze)
 
 first=maze()
-first.make_maze(2,2)
+first.make_maze(5,.1)
