5c0108ff2c049a5948f3d9538d563e9ad6296b73
Shivank Agrawal <shivankagrawal10@gmail.com>
2021-02-04 10:34:16 -0500

    added James files

---
+++ FireMaze_p1/firemaze.py
@@ -0,0 +1,206 @@
+import numpy as np
+import random
+import heapq
+import math
+import constants
+
+class maze:
+
+	def __init__(self, dim : int, p : float, q : float):
+		self.dim = dim
+		self.p = p
+		self.q = q
+		self.grid = np.zeros((dim, dim))
+		#random.seed(123)
+		for i in range(dim):
+			for j in range(dim):
+				if random.random() <= p:
+					self.grid[i][j] = constants.BLOCKED
+		self.grid[0][0] = constants.OPEN
+		self.grid[dim - 1][dim - 1] = constants.OPEN
+
+	def DFS(self, start : (int,int), end : (int, int)):
+		fringe = []
+		visited = {}
+		fringed = {}
+		fringe.append(start)
+		fringed[start] = True
+		while fringe:
+			curr = fringe.pop()
+			del fringed[curr]
+			visited[curr] = True
+			if curr == end:
+				return True
+			else:
+				neighbors = self.get_neighbors(curr, self.is_open)
+				while True:
+					try:
+						neighbor = next(neighbors)
+						if neighbor not in visited and neighbor not in fringed:
+							fringe.append(neighbor)
+							fringed[neighbor] = True
+					except StopIteration:
+						break        
+		return False
+
+	def Astar(self, start : (int, int), end : (int, int)):
+		fringe = []
+		visited = {}
+		predecessors = {}
+		heapq.heappush(fringe, (0, (0, start, constants.UNDEFINED)))
+		while fringe:
+			_, (cost, curr, pred) = heapq.heappop(fringe)
+			if curr in visited:
+				continue
+			else:
+				visited[curr] = True
+				predecessors[curr] = pred
+				if curr == end:
+					path = self.build_path(end, predecessors)
+					return (path, len(visited))
+				else:
+					neighbors = self.get_neighbors(curr, self.is_open)
+					while True:
+						try:
+							neighbor = next(neighbors)
+							if neighbor not in visited:
+								move_cost = cost + 1
+								priority = move_cost + self.get_dist_to(end, neighbor)
+								heapq.heappush(fringe, (priority, (move_cost, neighbor, curr)))
+						except StopIteration:
+							break
+		return ([], constants.NO_PATH)
+
+	def BFS(self, start : (int, int), end : (int, int)):
+		fringe = []
+		visited = {}
+		fringed = {}
+		predecessors = {}
+		predecessors[start] = constants.UNDEFINED
+		fringe.append(start)
+		fringed[start] = True
+		while fringe:
+			curr = fringe.pop(0)
+			del fringed[curr]
+			visited[curr] = True
+			if curr == end:
+				path = self.build_path(end, predecessors)
+				return (path, len(visited))
+			else:
+				neighbors = self.get_neighbors(curr, self.is_open)
+				while True:
+					try:
+						neighbor = next(neighbors)
+						if neighbor not in visited and neighbor not in fringed:
+							fringe.append(neighbor)
+							fringed[neighbor] = True
+							predecessors[neighbor] = curr
+					except StopIteration: 
+						break
+		return ([], constants.NO_PATH)
+
+	def ADAAC(self, start : (int, int), end : (int, int)):
+		fringe = []
+		visited = {}
+		predecessors = {}
+		heapq.heappush(fringe, (0, (0, start, constants.UNDEFINED)))
+		while fringe:
+			_, (travel_cost, curr, pred) = heapq.heappop(fringe)
+			if curr in visited:
+				continue
+			else:
+				visited[curr] = True
+				predecessors[curr] = pred
+				if curr == end:
+					path = self.build_path(end, predecessors)
+					return (path, len(visited))
+				else:
+					neighbors = self.get_neighbors(curr, self.is_open)
+					while True:
+						try:
+							neighbor = next(neighbors)
+							if neighbor not in visited:
+								move_cost = cost + 1
+								priority = move_cost + self.get_dist_to(end, neighbor) + self.get_fire_cost(neighbor)
+								heapq.heappush(fringe, (priority, (move_cost, neighbor, curr))) 
+						except StopIteration:
+							break
+		return ([], constants.NO_PATH)
+
+	def valid_cell(self, coordinate):
+		if ((coordinate[0] < 0 or coordinate[1] < 0) 
+		or (coordinate[0] >= self.dim or coordinate[1] >= self.dim)):
+			return False
+		else:
+			return True
+
+	def is_open(self, coordinate):
+		valid_cell = self.valid_cell(coordinate)
+		y, x = coordinate
+		if valid_cell and self.grid[y][x] != constants.BLOCKED and self.grid[y][x] != constants.FIRE:
+			return True
+		else:
+			return False
+
+	def on_fire(self, coordinate):
+		valid_cell = self.valid_cell(coordinate)
+		y, x = coordinate
+		if valid_cell and self.grid[y][x] == constants.FIRE:
+			return True
+		else:
+			return False 
+
+	def get_neighbors(self, curr : (int, int), predicate):
+		x = curr[1]
+		y = curr[0]
+		neighbors = filter(predicate, [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)])
+		return neighbors
+
+	def get_dist_to(self, end : (int, int), start : (int, int)):
+		goal_x = end[1]
+		goal_y = end[0]
+		start_x = start[1]
+		start_y = start[0]
+		euclid_dist = math.sqrt((goal_y - start_y) ** 2 + (goal_x - start_x) ** 2)
+		return euclid_dist
+
+	def build_path(self, end : (int, int), predecessors):
+		path = [end]
+		prev = predecessors[end]
+		while prev != constants.UNDEFINED:
+			path.insert(0, prev)
+			prev = predecessors[prev]
+		return path
+
+	def clone_grid(self):
+		clone = np.zeros((self.dim, self.dim))
+		for i in range(self.dim):
+			for j in range(self.dim):
+				clone[i][j] = self.grid[i][j]
+		return clone
+
+	def get_fire_prob(self, move : (int, int)):
+		neighbors = self.get_neighbors(move, self.on_fire)
+		k = 0
+		prob = 0
+		while True:
+			try:
+				_ = next(neighbors)
+				k = k + 1
+			except StopIteration:
+				break
+		if k != 0:
+			prob = 1 - (1 - self.q) ** k
+		return prob
+
+#first = maze(100, 0.3)
+#print(first.maze)
+#dfs = first.DFS((0, 0), (first.dim - 1, first.dim - 1))
+#bfs = first.BFS((0, 0), (first.dim - 1, first.dim - 1))
+#astar = first.Astar((0,0), (first.dim - 1, first.dim - 1))
+
+#print(dfs)
+#print(bfs)
+#print(len(bfs[0]))
+#print(astar)
+#print(len(astar[0]))
---
+++ FireMaze_p1/firemaze_runner.py
@@ -0,0 +1,86 @@
+import numpy as np
+import random
+import heapq
+import math
+import maze_generator as mg
+import constants
+
+class experiment:
+
+	def __init__(self, dim : int, p : float, q : float, start : (int, int), end : (int, int), strategy):
+		self.maze = mg.maze(dim, p, q)
+		self.q = q
+		self.start = start
+		self.end = end
+		y, x = start
+		self.agent = (y, x)
+		self.maze.grid[y][x] = constants.AGENT
+		self.strategy = strategy
+
+	def run(self):
+		self.start_fire()
+		plan = []
+		while self.agent != self.end:
+			input("Press any key to continue")
+			print(self.maze.grid)
+			plan = self.advance_agent(self.strategy, plan)
+			print("Agent moves to", self.agent)
+			if not plan:
+				break
+			new_grid = self.advance_fire()
+			print("New Maze:")
+			print(new_grid)
+			y, x = self.agent
+			if new_grid[y][x] == constants.FIRE:
+				break
+			self.maze.grid = new_grid
+		if self.agent == self.end:
+			print("Success")
+		else:
+			print("Failure")
+
+	def advance_agent(self, strategy, plan):
+		if strategy == constants.STRATEGY_1 and not plan:
+			plan, _ = self.maze.BFS(self.agent, self.end)
+		elif strategy == constants.STRATEGY_2:
+			plan, _ = self.maze.BFS(self.agent, self.end)
+		elif strategy == constants.STRATEGY_3:
+			plan, _ = self.maze.ADAAC(self.agent, self.end)
+		if plan:
+			plan.pop(0)
+			y, x = plan[0]
+			if self.maze.grid[y][x] == constants.FIRE:
+				plan = []
+			else:
+				old_y, old_x = self.agent
+				new_y, new_x = self.agent = plan[0]
+				self.maze.grid[old_y][old_x] = constants.OPEN
+				self.maze.grid[new_y][new_x] = constants.AGENT
+		return plan
+
+	def advance_fire(self):
+		clone = self.maze.clone_grid()
+		for i in range(self.maze.dim):
+			for j in range(self.maze.dim):
+				if clone[i][j] != constants.FIRE and clone[i][j] != constants.BLOCKED:
+					prob = self.maze.get_fire_prob((i, j))
+					#print(i, ",", j, "has the following:")
+					#print(self.q)
+					#print(prob)
+					if prob != 0 and random.random() <= prob:
+						clone[i][j] = constants.FIRE
+		return clone
+
+	def start_fire(self):
+		open_cells = []
+		for i in range(self.maze.dim):
+			for j in range(self.maze.dim):
+				if self.maze.grid[i][j] == constants.OPEN:
+					open_cells.append((i, j))
+		y, x = random.choice(open_cells)
+		self.maze.grid[y][x] = constants.FIRE
+
+exp = experiment(5, .2 , 1, (0, 0), (4, 4), 1)
+exp.run()
+
+
