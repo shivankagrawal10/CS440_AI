3d2c3d6a8ee5b38d134fb04267d9f41239cb2897
Shivank Agrawal <shivankagrawal10@gmail.com>
2021-02-12 12:37:03 -0500

    added git ignore

--- FireMaze_p1/firemaze.py
+++ FireMaze_p1/firemaze.py
@@ -24,168 +24,168 @@ class maze:
 		self.qs = [i * .1 for i in range(10, 0, -1) if (i * .1) >= self.q]
 
 
-    def DFS(self, start : (int,int), end : (int, int)):
-        fringe = []
-        visited = {}
-        fringed = {}
-        fringe.append(start)
-        fringed[start] = True
-        while fringe:
-            curr = fringe.pop()
-            del fringed[curr]
-            visited[curr] = True
-            if curr == end:
-                return True
-            else:
-                neighbors = self.get_neighbors(curr, self.is_open)
-                while True:
-                    try:
-                        neighbor = next(neighbors)
-                        if neighbor not in visited and neighbor not in fringed:
-                            fringe.append(neighbor)
-                            fringed[neighbor] = True
-                    except StopIteration:
-                        break        
-        return False
+	def DFS(self, start : (int,int), end : (int, int)):
+		fringe = []
+		visited = {}
+		fringed = {}
+		fringe.append(start)
+		fringed[start] = True
+		while fringe:
+			curr = fringe.pop()
+			del fringed[curr]
+			visited[curr] = True
+			if curr == end:
+				return True
+			else:
+				neighbors = self.get_neighbors(curr, self.is_open)
+				while True:
+					try:
+						neighbor = next(neighbors)
+						if neighbor not in visited and neighbor not in fringed:
+							fringe.append(neighbor)
+							fringed[neighbor] = True
+					except StopIteration:
+						break        
+		return False
 
-    def Astar(self, start : (int, int), end : (int, int)):
-        fringe = []
-        visited = {}
-        predecessors = {}
-        heapq.heappush(fringe, (0, (0, start, constants.UNDEFINED)))
-        while fringe:
-            _, (cost, curr, pred) = heapq.heappop(fringe)
-            if curr in visited:
-                continue
-            else:
-                visited[curr] = True
-                predecessors[curr] = pred
-                if curr == end:
-                    path = self.build_path(end, predecessors)
-                    return (path, len(visited))
-                else:
-                    neighbors = self.get_neighbors(curr, self.is_open)
-                    while True:
-                        try:
-                            neighbor = next(neighbors)
-                            if neighbor not in visited:
-                                move_cost = cost + 1
-                                priority = move_cost + self.get_dist_to(end, neighbor)
-                                heapq.heappush(fringe, (priority, (move_cost, neighbor, curr)))
-                        except StopIteration:
-                            break
-        return ([], constants.NO_PATH)
+	def Astar(self, start : (int, int), end : (int, int)):
+		fringe = []
+		visited = {}
+		predecessors = {}
+		heapq.heappush(fringe, (0, (0, start, constants.UNDEFINED)))
+		while fringe:
+			_, (cost, curr, pred) = heapq.heappop(fringe)
+			if curr in visited:
+				continue
+			else:
+				visited[curr] = True
+				predecessors[curr] = pred
+				if curr == end:
+					path = self.build_path(end, predecessors)
+					return (path, len(visited))
+				else:
+					neighbors = self.get_neighbors(curr, self.is_open)
+					while True:
+						try:
+							neighbor = next(neighbors)
+							if neighbor not in visited:
+								move_cost = cost + 1
+								priority = move_cost + self.get_dist_to(end, neighbor)
+								heapq.heappush(fringe, (priority, (move_cost, neighbor, curr)))
+						except StopIteration:
+							break
+		return ([], constants.NO_PATH)
 
-    def BFS(self, start : (int, int), end : (int, int)):
-        fringe = []
-        visited = {}
-        fringed = {}
-        predecessors = {}
-        predecessors[start] = constants.UNDEFINED
-        fringe.append(start)
-        fringed[start] = True
-        while fringe:
-            curr = fringe.pop(0)
-            del fringed[curr]
-            visited[curr] = True
-            if curr == end:
-                path = self.build_path(end, predecessors)
-                return (path, len(visited))
-            else:
-                neighbors = self.get_neighbors(curr, self.is_open)
-                while True:
-                    try:
-                        neighbor = next(neighbors)
-                        if neighbor not in visited and neighbor not in fringed:
-                            fringe.append(neighbor)
-                            fringed[neighbor] = True
-                            predecessors[neighbor] = curr
-                    except StopIteration: 
-                        break
-        return ([], constants.NO_PATH)
+	def BFS(self, start : (int, int), end : (int, int)):
+		fringe = []
+		visited = {}
+		fringed = {}
+		predecessors = {}
+		predecessors[start] = constants.UNDEFINED
+		fringe.append(start)
+		fringed[start] = True
+		while fringe:
+			curr = fringe.pop(0)
+			del fringed[curr]
+			visited[curr] = True
+			if curr == end:
+				path = self.build_path(end, predecessors)
+				return (path, len(visited))
+			else:
+				neighbors = self.get_neighbors(curr, self.is_open)
+				while True:
+					try:
+						neighbor = next(neighbors)
+						if neighbor not in visited and neighbor not in fringed:
+							fringe.append(neighbor)
+							fringed[neighbor] = True
+							predecessors[neighbor] = curr
+					except StopIteration: 
+						break
+		return ([], constants.NO_PATH)
 
-    def ADAAC(self, start : (int, int), end : (int, int)):
-        fringe = []
-        visited = {}
-        predecessors = {}
-        heapq.heappush(fringe, (0, (0, start, constants.UNDEFINED)))
-        while fringe:
-            _, (travel_cost, curr, pred) = heapq.heappop(fringe)
-            if curr in visited:
-                continue
-            else:
-                visited[curr] = True
-                predecessors[curr] = pred
-                if curr == end:
-                    path = self.build_path(end, predecessors)
-                    return (path, len(visited))
-                else:
-                    neighbors = self.get_neighbors(curr, self.is_open)
-                    while True:
-                        try:
-                            neighbor = next(neighbors)
-                            if neighbor not in visited:
-                                move_cost = cost + 1
-                                priority = move_cost + self.get_dist_to(end, neighbor) + self.get_fire_cost(neighbor)
-                                heapq.heappush(fringe, (priority, (move_cost, neighbor, curr))) 
-                        except StopIteration:
-                            break
-        return ([], constants.NO_PATH)
-    
-    def Marco_Polo(self, start : (int, int), end : (int, int)):
-        fringe = []
-        visited = {}
-        predecessors = {}
-        heapq.heappush(fringe, (0, (0, start, constants.UNDEFINED)))
-        while fringe:
-            _, (cost, curr, pred) = heapq.heappop(fringe)
-            if curr in visited:
-                continue
-            else:
-                visited[curr] = True
-                predecessors[curr] = pred
-                if curr == end:
-                    path = self.build_path(end, predecessors)
-                    return (path, len(visited))
-                else:
-                    neighbors = self.get_neighbors(curr, self.is_open)
-                    while True:
-                        try:
-                            neighbor = next(neighbors)
-                            if neighbor not in visited:
-                                move_cost = cost + 1
-                                dist_to_fire = self.dist_to_nearest_fire(neighbor)
-                                priority = move_cost + self.get_dist_to(end, neighbor) - dist_to_fire
-                                heapq.heappush(fringe, (priority, (move_cost, neighbor, curr)))
-                        except StopIteration:
-                            break
-        return ([], constants.NO_PATH)
-    
-    def CTF(self, start : (int, int), end : (int, int)):
-        real_grid = self.grid
-        real_q = self.q
-        plan = []
-        signal = 0
-        while True:
-            q = self.qs[0]
-            self.q = q
-            self.grid = self.pred_fire()
-            plan, signal = self.Astar(start, end)
-            #print("Q is ", self.q)
-            #print(plan)
-            if plan:
-                break
-            else:
-                if q != real_q:
-                    self.qs.pop(0)
-                else:
-                    break
-            self.grid = real_grid
-        self.grid = real_grid
-        self.q = real_q
-        if not plan:
-            plan, signal = self.Astar(start, end)
-        return (plan, signal)
+	def ADAAC(self, start : (int, int), end : (int, int)):
+		fringe = []
+		visited = {}
+		predecessors = {}
+		heapq.heappush(fringe, (0, (0, start, constants.UNDEFINED)))
+		while fringe:
+			_, (travel_cost, curr, pred) = heapq.heappop(fringe)
+			if curr in visited:
+				continue
+			else:
+				visited[curr] = True
+				predecessors[curr] = pred
+				if curr == end:
+					path = self.build_path(end, predecessors)
+					return (path, len(visited))
+				else:
+					neighbors = self.get_neighbors(curr, self.is_open)
+					while True:
+						try:
+							neighbor = next(neighbors)
+							if neighbor not in visited:
+								move_cost = cost + 1
+								priority = move_cost + self.get_dist_to(end, neighbor) + self.get_fire_cost(neighbor)
+								heapq.heappush(fringe, (priority, (move_cost, neighbor, curr))) 
+						except StopIteration:
+							break
+		return ([], constants.NO_PATH)
+	
+	def Marco_Polo(self, start : (int, int), end : (int, int)):
+		fringe = []
+		visited = {}
+		predecessors = {}
+		heapq.heappush(fringe, (0, (0, start, constants.UNDEFINED)))
+		while fringe:
+			_, (cost, curr, pred) = heapq.heappop(fringe)
+			if curr in visited:
+				continue
+			else:
+				visited[curr] = True
+				predecessors[curr] = pred
+				if curr == end:
+					path = self.build_path(end, predecessors)
+					return (path, len(visited))
+				else:
+					neighbors = self.get_neighbors(curr, self.is_open)
+					while True:
+						try:
+							neighbor = next(neighbors)
+							if neighbor not in visited:
+								move_cost = cost + 1
+								dist_to_fire = self.dist_to_nearest_fire(neighbor)
+								priority = move_cost + self.get_dist_to(end, neighbor) - dist_to_fire
+								heapq.heappush(fringe, (priority, (move_cost, neighbor, curr)))
+						except StopIteration:
+							break
+		return ([], constants.NO_PATH)
+	
+	def CTF(self, start : (int, int), end : (int, int)):
+		real_grid = self.grid
+		real_q = self.q
+		plan = []
+		signal = 0
+		while True:
+			q = self.qs[0]
+			self.q = q
+			self.grid = self.pred_fire()
+			plan, signal = self.Astar(start, end)
+			#print("Q is ", self.q)
+			#print(plan)
+			if plan:
+				break
+			else:
+				if q != real_q:
+					self.qs.pop(0)
+				else:
+					break
+			self.grid = real_grid
+		self.grid = real_grid
+		self.q = real_q
+		if not plan:
+			plan, signal = self.Astar(start, end)
+		return (plan, signal)
 
 	def Marco_Polo_Future(self, start : (int, int), end : (int, int)):
 		fringe = []
@@ -220,103 +220,103 @@ class maze:
 		self.grid = real_grid
 		return ([], constants.NO_PATH)
 
-	def maze_visualize(self, agent, grid):
+	def maze_visualize(self, agent, grid,show):
 		#colors={0:'white',1:'black',2:'red'}
 		cmap = mat.colors.LinearSegmentedColormap.from_list("", ["skyblue","gray","red","white"])
 		plt.imshow(grid,cmap)
 		plt.draw()
-		if(agent == self.end):
+		if(show==1):
 			plt.show()
 		else:
-			plt.pause(3)
+			plt.pause(.1)
 		return
 
 	def nearest_fire(self, curr:(int, int)):
 		return min(self.fireloc, key = lambda x: abs(x[1] - curr[1]) + abs(x[0] - curr[0]))
 
-    def dist_to_nearest_fire(self, curr: (int, int)):
-        fire = self.nearest_fire(curr)
-        dist = abs(fire[1] - curr[1]) + abs(fire[0] - curr[0])
-        return dist
-        
-    def valid_cell(self, coordinate):
-        if ((coordinate[0] < 0 or coordinate[1] < 0) 
-        or (coordinate[0] >= self.dim or coordinate[1] >= self.dim)):
-            return False
-        else:
-            return True
+	def dist_to_nearest_fire(self, curr: (int, int)):
+		fire = self.nearest_fire(curr)
+		dist = abs(fire[1] - curr[1]) + abs(fire[0] - curr[0])
+		return dist
+		
+	def valid_cell(self, coordinate):
+		if ((coordinate[0] < 0 or coordinate[1] < 0) 
+		or (coordinate[0] >= self.dim or coordinate[1] >= self.dim)):
+			return False
+		else:
+			return True
 
-    def is_open(self, coordinate):
-        valid_cell = self.valid_cell(coordinate)
-        y, x = coordinate
-        if valid_cell and self.grid[y][x] != constants.BLOCKED and self.grid[y][x] != constants.FIRE:
-            return True
-        else:
-            return False
+	def is_open(self, coordinate):
+		valid_cell = self.valid_cell(coordinate)
+		y, x = coordinate
+		if valid_cell and self.grid[y][x] != constants.BLOCKED and self.grid[y][x] != constants.FIRE:
+			return True
+		else:
+			return False
 
-    def on_fire(self, coordinate):
-        valid_cell = self.valid_cell(coordinate)
-        y, x = coordinate
-        if valid_cell and self.grid[y][x] == constants.FIRE:
-            return True
-        else:
-            return False 
+	def on_fire(self, coordinate):
+		valid_cell = self.valid_cell(coordinate)
+		y, x = coordinate
+		if valid_cell and self.grid[y][x] == constants.FIRE:
+			return True
+		else:
+			return False 
 
-    def get_neighbors(self, curr : (int, int), predicate):
-        x = curr[1]
-        y = curr[0]
-        neighbors = filter(predicate, [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)])
-        return neighbors
+	def get_neighbors(self, curr : (int, int), predicate):
+		x = curr[1]
+		y = curr[0]
+		neighbors = filter(predicate, [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)])
+		return neighbors
 
-    def get_dist_to(self, end : (int, int), start : (int, int)):
-        goal_x = end[1]
-        goal_y = end[0]
-        start_x = start[1]
-        start_y = start[0]
-        euclid_dist = math.sqrt((goal_y - start_y) ** 2 + (goal_x - start_x) ** 2)
-        return euclid_dist
+	def get_dist_to(self, end : (int, int), start : (int, int)):
+		goal_x = end[1]
+		goal_y = end[0]
+		start_x = start[1]
+		start_y = start[0]
+		euclid_dist = math.sqrt((goal_y - start_y) ** 2 + (goal_x - start_x) ** 2)
+		return euclid_dist
 
-    def build_path(self, end : (int, int), predecessors):
-        path = [end]
-        prev = predecessors[end]
-        while prev != constants.UNDEFINED:
-            path.insert(0, prev)
-            prev = predecessors[prev]
-        return path
+	def build_path(self, end : (int, int), predecessors):
+		path = [end]
+		prev = predecessors[end]
+		while prev != constants.UNDEFINED:
+			path.insert(0, prev)
+			prev = predecessors[prev]
+		return path
 
-    def clone_grid(self):
-        clone = np.zeros((self.dim, self.dim))
-        for i in range(self.dim):
-            for j in range(self.dim):
-                clone[i][j] = self.grid[i][j]
-        return clone
+	def clone_grid(self):
+		clone = np.zeros((self.dim, self.dim))
+		for i in range(self.dim):
+			for j in range(self.dim):
+				clone[i][j] = self.grid[i][j]
+		return clone
 
-    def get_fire_prob(self, move : (int, int)):
-        neighbors = self.get_neighbors(move, self.on_fire)
-        k = 0
-        prob = 0
-        while True:
-            try:
-                _ = next(neighbors)
-                k = k + 1
-            except StopIteration:
-                break
-        if k != 0:
-            prob = 1 - (1 - self.q) ** k
-        return prob
+	def get_fire_prob(self, move : (int, int)):
+		neighbors = self.get_neighbors(move, self.on_fire)
+		k = 0
+		prob = 0
+		while True:
+			try:
+				_ = next(neighbors)
+				k = k + 1
+			except StopIteration:
+				break
+		if k != 0:
+			prob = 1 - (1 - self.q) ** k
+		return prob
 
-    def pred_fire(self):
-        clone = self.clone_grid()
-        for i in range(self.dim):
-            for j in range(self.dim):
-                if clone[i][j] != constants.FIRE and clone[i][j] != constants.BLOCKED:
-                    prob = self.get_fire_prob((i, j))
-                    #print(i, ",", j, "has the following:")
-                    #print(self.q)
-                    #print(prob)
-                    if prob != 0 and random.random() <= prob:
-                        clone[i][j] = constants.FIRE
-        return clone
+	def pred_fire(self):
+		clone = self.clone_grid()
+		for i in range(self.dim):
+			for j in range(self.dim):
+				if clone[i][j] != constants.FIRE and clone[i][j] != constants.BLOCKED:
+					prob = self.get_fire_prob((i, j))
+					#print(i, ",", j, "has the following:")
+					#print(self.q)
+					#print(prob)
+					if prob != 0 and random.random() <= prob:
+						clone[i][j] = constants.FIRE
+		return clone
 
 #first = maze(100, 0.3)
 #print(first.maze)
