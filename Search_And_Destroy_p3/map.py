d58e746d0e6b09cc1789cb5c6eaea41be716e8d6
jameslavery <jlavery416@gmail.com>
2021-04-06 13:31:07 -0400

    map

--- Search_And_Destroy_p3/map.py
+++ Search_And_Destroy_p3/map.py
@@ -1,12 +1,38 @@
 import numpy as np
+import random
+import constants
 
 class Map:
 	def __init__(self, dim):
-		grid = self.create_grid(dim)
+		self.grid = self.create_grid(dim)
+		self.dim = dim
 
 	def create_grid(self, dim):
 		grid = np.zeros((dim, dim))
 		for i in range(dim):
 			for j in range(dim):
+				p = random.random()
+				if p < constants.flat_cell_prob:
+					grid[i][j] = constants.flat
+				elif p < constants.hill_cell_prob:
+					grid[i][j] = constants.hilly
+				elif p < constants.forest_cell_prob:
+					grid[i][j] = constants.forested
+				elif p < constants.maze_of_caves_prob:
+					grid[i][j] = constants.maze_of_caves
+		return grid
+
+	def print_map(self):
+		printable = ""
+		for i in range(self.dim):
+			for j in range(self.dim):
+				printable += str(int(self.grid[i][j])) + " "
+
+			printable += "\n"
+		print(printable)
+
+test = Map(10)
+test.print_map()
+
 				
 
