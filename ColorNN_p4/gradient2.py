da75b8ff0270b544f775cc7ed3db854e13a0266e
Shivank Agrawal <shivankagrawal10@gmail.com>
2021-04-30 16:25:06 -0400

    added improved

---
+++ ColorNN_p4/gradient2.py
@@ -0,0 +1,18 @@
+import numpy as np
+import math
+
+def f(w, x):
+	return 1 * (((np.arctan(np.dot(w,x)))/math.pi) + .5)
+
+def df(w, x):
+	return (-1/math.pi) * (1/(1 + np.dot(w,x)**2)) * x
+
+def dL(w, x, y):
+	return ((2 * (y - f(w, x))) * df(w, x))
+
+def w_tplus(w, x, y, alpha):
+	return w - (alpha * dL(w,x,y))
+
+def L(w, x, y):
+	return (y - f(w, x))**2
+
